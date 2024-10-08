7ed3abf837df18ccff24e8f1b33b9af64c1f9a23
vivek kumar <vivekkumarsharma7870@gmail.com>
2024-10-08 14:51:31 +0530

    submitting ques 1

--- program1.py
+++ program1.py
@@ -1,10 +1,29 @@
 class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+    def isValid(self, s:str):
+        stack = []
+        opening = set('([{')
+        closing = set(')]}')
+        pair = {')' : '(',']' : '[', '}' : '{'}
+
+        for i in s:
+            if i in opening:
+                stack.append(i)
+            if i in closing:
+                if not stack:
+                    return False
+                elif stack.pop() != pair[i]:
+                    return False
+                else:
+                    continue
+        if not stack:
+            return True
+        else:
+            return False
+
+obj1 = Solution()
+print(obj1.isValid("{[]}"))
+
+
 
 
 
